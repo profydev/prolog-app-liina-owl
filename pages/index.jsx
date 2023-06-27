73f31d217250416a623755cd929b643a87f94a14
Liina <90990795+liina-owl@users.noreply.github.com>
2023-06-27 04:01:16 +0000

    Header and Contact MOdal

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Routes } from "@config/routes";
+import { useState } from "react";
 
 const Header = styled.header`
   width: 100%;
@@ -28,24 +29,142 @@ const ContactButton = styled.button`
   }
 `;
 
+const PrimaryButton = styled.a`
+  border-radius: 8px;
+  border: 1px solid var(--primary-600, #7f56d9);
+  background: var(--primary-600, #7f56d9);
+  /* Shadow/xs */
+  box-shadow: 0px 1px 2px 0px rgba(16, 24, 40, 0.05);
+  display: flex;
+  padding: 10px 18px;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  color: #ffffff;
+  text-decoration: none;
+`;
+
+const HeaderLink = styled.a`
+  display: flex;
+  align-items: flex-start;
+  color: #667085;
+  text-decoration: none;
+`;
+
+const LinksContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 32px;
+`;
+
+const ModalContainer = styled.div`
+  width: 100%;
+`;
+
+const ModalCard = styled.div`
+  margin: auto;
+  display: flex;
+  width: 400px;
+  padding: 24px;
+  flex-direction: column;
+  align-items: center;
+  gap: 32px;
+  border-radius: 12px;
+  background: var(--white, #fff);
+  /* Shadow/xl */
+  box-shadow: 0px 8px 8px -4px rgba(16, 24, 40, 0.04),
+    0px 20px 24px -4px rgba(16, 24, 40, 0.1);
+`;
+
+const ModalHeader = styled.h1`
+  color: var(--gray-900, #101828);
+  text-align: center;
+  /* Text lg/Medium */
+  font-size: 18px;
+  font-family: Inter;
+  font-weight: 500;
+  line-height: 28px;
+  display: flex;
+  flex-direction: column;
+  align-self: stretch;
+`;
+
+const ModalBody = styled.p`
+  display: flex;
+  flex-direction: column;
+  align-self: stretch;
+  color: var(--gray-500, #667085);
+  text-align: center;
+  /* Text sm/Normal */
+  font-size: 14px;
+  font-family: Inter;
+  line-height: 20px;
+`;
+
+const CancelButton = styled.button`
+  display: flex;
+  padding: 10px 18px;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  flex: 1 0 0;
+  border-radius: 8px;
+  border: 1px solid var(--gray-300, #d0d5dd);
+  background: var(--white, #fff);
+  /* Shadow/xs */
+  box-shadow: 0px 1px 2px 0px rgba(16, 24, 40, 0.05);
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 18px;
+`;
+
 const IssuesPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  // https://profy.dev/rjs-challenge-modal
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const ContactModal = () => {
+    const emailLink = `mailto:"prolog@profy.dev"`;
+    return (
+      <ModalContainer>
+        <ModalCard>
+          <ModalHeader>Contact Us Via Email</ModalHeader>
+          <ModalBody>
+            Any questions? Send us an email at prolog@profy.dev. We usually
+            answer within 24 hours.
+          </ModalBody>
+          <ButtonContainer>
+            <CancelButton onClick={toggleModal}>Cancel</CancelButton>
+            <PrimaryButton href={emailLink}>Open Email App</PrimaryButton>
+          </ButtonContainer>
+        </ModalCard>
+      </ModalContainer>
+    );
+  };
+
   return (
     <div>
       <Header>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/icons/logo-large.svg" alt="Prolog logo" />
-        <a href={Routes.projects}>Dashboard</a>
+        <LinksContainer>
+          <HeaderLink href="/">Home</HeaderLink>
+          <HeaderLink href="/products">Products</HeaderLink>
+          <HeaderLink href="/docuentation">Documentation</HeaderLink>
+          <HeaderLink href="pricing">Pricing</HeaderLink>
+        </LinksContainer>
+        <PrimaryButton href="/projects">Open Dashboard</PrimaryButton>
       </Header>
-      <ContactButton
-        onClick={() =>
-          alert(
-            "Implement this in Challenge 2 - Modal:\n\nhttps://profy.dev/rjs-challenge-modal"
-          )
-        }
-      >
+      <ContactButton onClick={toggleModal}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/icons/message.svg" alt="Contact" />
       </ContactButton>
+      {isOpen && <ContactModal />}
     </div>
   );
 };
